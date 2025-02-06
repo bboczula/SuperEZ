99b011f82496a5304e3feb8107d974a0e5dd9c1f
Bartosz Boczula <boczula.bartosz@gmail.com>
2025-02-06 19:21:23 +0100

    Added Camera Constant Buffer to the shader
    Added Camera buffer to the shader, bound to the position of the Inline Root Constant. Add the multiplication of the geometry by that camera view-projection matrix

--- source/engine/shaders/shaders.hlsl
+++ source/engine/shaders/shaders.hlsl
@@ -8,6 +8,10 @@
 // PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 //
 //*********************************************************
+cbuffer CameraData : register(b0)
+{
+	float4x4 viewProjection;
+};
 
 struct PSInput
 {
@@ -20,7 +24,7 @@ PSInput VSMain(float4 position : POSITION, float4 color : COLOR)
 {
     PSInput result;
 
-    result.position = position;
+    result.position = mul(position, viewProjection);
     result.color = color;
 
     return result;
