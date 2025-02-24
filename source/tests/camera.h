3642b5a4febbe79035aebf971c49f119ee6fc57a
Bartosz Boczula <boczula.bartosz@gmail.com>
2025-02-24 05:58:59 +0100

    First failing Unit Test
    First Unit Test, failing, just to see if I've integrated everything correctly.

---
+++ source/tests/camera.h
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(CameraTestSuite, ThisTestWillFail)
+{
+	ASSERT_EQ(1, 2);
+}
--- source/tests/main.cpp
+++ source/tests/main.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "camera.h"
+
 int main(int argc, char** argv)
 {
 	testing::InitGoogleTest(&argc, argv);
